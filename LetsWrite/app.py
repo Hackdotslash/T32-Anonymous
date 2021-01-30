ae4c00e0b09a37f67ea589b3cd07882846863a60
jainil-4801 <201851116@iiitvadodara.ac.in>
2021-01-30 17:28:44 +0530

    updating backend with integrating to Twillo

--- LetsWrite/app.py
+++ LetsWrite/app.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, abort, redirect
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant, ChatGrant
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 app = Flask(__name__)
 
@@ -5,6 +14,43 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.get_json(force=True).get('username')
+    if not username:
+        abort(401)
+
+    conversation = get_chatroom('My Room')
+    try:
+        conversation.participants.create(identity=username)
+    except TwilioRestException as exc:
+        # do not error if the user is already in the conversation
+        if exc.status != 409:
+            raise
+
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid,
+                        twilio_api_key_secret, identity=username)
+    token.add_grant(VideoGrant(room='My Room'))
+    token.add_grant(ChatGrant(service_sid=conversation.chat_service_sid))
+
+    return {'token': token.to_jwt().decode(),
+            'conversation_sid': conversation.sid}
+
+# Code Changes from here 
+
+@app.route('/token')
+def token():
+    # Get credentials for environment variables
+
+    # Create an Access Token
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid, twilio_api_key_secret)
+
+    # Set the Identity of this token
+    token.identity = request.values.get('identity') or 'identity'
+
+    # Return token
+    return token.to_jwt()
 
 
 if __name__ == '__main__':
