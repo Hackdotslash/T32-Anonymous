cb5acce22ccccb2ba6f921d9f78e718f3cee2a14
Adit Alware <42518974+aditalware@users.noreply.github.com>
2021-01-30 21:10:57 +0530

    updating app.py

--- LetsWrite/app.py
+++ LetsWrite/app.py
@@ -1,320 +1,57 @@
-const root = document.getElementById('root');
-const usernameInput = document.getElementById('username');
-const button = document.getElementById('join_leave');
-const shareScreen = document.getElementById('share_screen');
-const toggleChat = document.getElementById('toggle_chat');
-const container = document.getElementById('container');
-const count = document.getElementById('count');
-const chatScroll = document.getElementById('chat-scroll');
-const chatContent = document.getElementById('chat-content');
-const chatInput = document.getElementById('chat-input');
-const lableInput = document.getElementById('one');
-const localDiv = document.getElementById('local');
-let connected = false;
-let room;
-let chat;
-let conv;
-let screenTrack;
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, abort, redirect
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant, ChatGrant
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+from twilio.twiml.messaging_response import MessagingResponse
 
 
-function addLocalVideo() {
-    Twilio.Video.createLocalVideoTrack().then(track => {
-        let video = document.getElementById('local').firstChild;
-        let trackElement = track.attach();
-        trackElement.addEventListener('click', () => { zoomTrack(trackElement); });
-        video.appendChild(trackElement);
-    });
-};
+app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.get_json(force=True).get('username')
+    if not username:
+        abort(401)
+
+    conversation = get_chatroom('My Room')
+    try:
+        conversation.participants.create(identity=username)
+    except TwilioRestException as exc:
+        # do not error if the user is already in the conversation
+        if exc.status != 409:
+            raise
+
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid,
+                        twilio_api_key_secret, identity=username)
+    token.add_grant(VideoGrant(room='My Room'))
+    token.add_grant(ChatGrant(service_sid=conversation.chat_service_sid))
+
+    return {'token': token.to_jwt().decode(),
+            'conversation_sid': conversation.sid}
+
+# Code Changes from here 
+
+@app.route('/token')
+def token():
+    # Get credentials for environment variables
+
+    # Create an Access Token
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid, twilio_api_key_secret)
+
+    # Set the Identity of this token
+    token.identity = request.values.get('identity') or 'identity'
+
+    # Return token
+    return token.to_jwt()
 
 
-function capture1() {
-    var canvas = document.getElementById('canvas');
-    var video = document.getElementById('video');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
-    var img1 = canvas.toDataURL();
-    $.ajax({
-                url: '/process1',
-                data: {
-                    imageBase64 : img1                    
-                },
-                type: 'POST',
-                success: function(data){
-                    // $("#result").text("Predicted Output : "+data);
-                    // canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight); 
-                    console.log("success!!")
-                    // console.log(data)
-                    conv.sendMessage(data);
-                } 
-            })
-
-    // conv.sendMessage("https://i.pinimg.com/originals/83/f9/37/83f937b69f30bb886ab8a03390da6771.jpg");
-};
-
-
-function assignMentor(event){
-    connectButtonHandler(event);
-    mentor = usernameInput.value;
-    console.log(mentor);
-    // Disable Mentor Button
-    document.getElementById('join_leave_mentor').style.display = "none";
-    document.getElementById('Turn').style.display = "none";
-    now_streaming = mentor;
-    
-}
-
-function assignStudent(event){
-    connectButtonHandler(event);
-    document.getElementById('join_leave_mentor').style.display = "none";
-    document.getElementById('Turn').style.display = "block";
-    document.getElementById('Allow').style.display = "none";
-    document.getElementById('cls').style.display = "none";
-    document.getElementById('turn_over').style.display = "none";
-}
-
-
-
-function connectButtonHandler(event) {
-    event.preventDefault();
-    if (!connected) {
-        let username = usernameInput.value;
-        if (!username) {
-            alert('Enter your name before connecting');
-            return;
-        }
-        button.disabled = true;
-        button.innerHTML = 'Connecting...';
-        connect(username).then(() => {
-            lableInput.innerHTML = username;
-            button.innerHTML = 'Leave call';
-            button.disabled = false;
-            shareScreen.disabled = false;
-            capture.disabled = false;
-        }).catch(() => {
-            alert('Connection failed. Is the backend running?');
-            button.innerHTML = 'Join call';
-            button.disabled = false;
-        });
-    }
-    else {
-        disconnect();
-        button.innerHTML = 'Join call';
-        connected = false;
-        shareScreen.innerHTML = 'Share screen';
-        shareScreen.disabled = true;
-        capture.disabled = true;
-    }
-};
-
-function connect(username) {
-    let promise = new Promise((resolve, reject) => {
-        // get a token from the back end
-        let data;
-        fetch('/login', {
-            method: 'POST',
-            body: JSON.stringify({'username': username})
-        }).then(res => res.json()).then(_data => {
-            // join video call
-            data = _data;
-            return Twilio.Video.connect(data.token);
-        }).then(_room => {
-            room = _room;
-            if(username != "Jainil"){
-                localDiv.className = "participantHidden";
-            }
-            room.participants.forEach(participantConnected);
-            room.on('participantConnected', participantConnected);
-            room.on('participantDisconnected', participantDisconnected);
-            connected = true;
-            updateParticipantCount();
-            connectChat(data.token, data.conversation_sid);
-            resolve();
-        }).catch(e => {
-            console.log(e);
-            reject();
-        });
-    });
-    return promise;
-};
-
-function updateParticipantCount() {
-    if (!connected)
-        count.innerHTML = 'Disconnected.';
-    else
-        count.innerHTML = (room.participants.size + 1) + ' participants online.';
-};
-
-function participantConnected(participant) {
-    let participantDiv = document.createElement('div');
-    participantDiv.setAttribute('id', participant.sid);
-    participantDiv.setAttribute('class',participant.identity=='Jainil'?'participant':'participantHidden');
-    let tracksDiv = document.createElement('div');
-    participantDiv.appendChild(tracksDiv);
-
-    let labelDiv = document.createElement('div');
-    labelDiv.setAttribute('class', 'label');
-    labelDiv.innerHTML = participant.identity;
-    participantDiv.appendChild(labelDiv);
-
-    container.appendChild(participantDiv);
-
-    participant.tracks.forEach(publication => {
-        if (publication.isSubscribed)
-            trackSubscribed(tracksDiv, publication.track);
-    });
-    participant.on('trackSubscribed', track => trackSubscribed(tracksDiv, track));
-    participant.on('trackUnsubscribed', trackUnsubscribed);
-
-    updateParticipantCount();
-};
-
-function participantDisconnected(participant) {
-    document.getElementById(participant.sid).remove();
-    updateParticipantCount();
-};
-
-function trackSubscribed(div, track) {
-    let trackElement = track.attach();
-    trackElement.addEventListener('click', () => { zoomTrack(trackElement); });
-    div.appendChild(trackElement);
-};
-
-function trackUnsubscribed(track) {
-    track.detach().forEach(element => {
-        if (element.classList.contains('participantZoomed')) {
-            zoomTrack(element);
-        }
-        element.remove()
-    });
-};
-
-function disconnect() {
-    room.disconnect();
-    if (chat) {
-        chat.shutdown().then(() => {
-            conv = null;
-            chat = null;
-        });
-    }
-    while (container.lastChild.id != 'local')
-        container.removeChild(container.lastChild);
-    button.innerHTML = 'Join call';
-    if (root.classList.contains('withChat')) {
-        root.classList.remove('withChat');
-    }
-    toggleChat.disabled = true;
-    connected = false;
-    updateParticipantCount();
-};
-
-function shareScreenHandler() {
-    event.preventDefault();
-    if (!screenTrack) {
-        navigator.mediaDevices.getDisplayMedia().then(stream => {
-            screenTrack = new Twilio.Video.LocalVideoTrack(stream.getTracks()[0]);
-            room.localParticipant.publishTrack(screenTrack);
-            screenTrack.mediaStreamTrack.onended = () => { shareScreenHandler() };
-            console.log(screenTrack);
-            shareScreen.innerHTML = 'Stop sharing';
-        }).catch(() => {
-            alert('Could not share the screen.')
-        });
-    }
-    else {
-        room.localParticipant.unpublishTrack(screenTrack);
-        screenTrack.stop();
-        screenTrack = null;
-        shareScreen.innerHTML = 'Share screen';
-    }
-};
-
-function zoomTrack(trackElement) {
-    if (!trackElement.classList.contains('trackZoomed')) {
-        // zoom in
-        container.childNodes.forEach(participant => {
-            if (participant.classList && participant.classList.contains('participant')) {
-                let zoomed = false;
-                participant.childNodes[0].childNodes.forEach(track => {
-                    if (track === trackElement) {
-                        track.classList.add('trackZoomed')
-                        zoomed = true;
-                    }
-                });
-                if (zoomed) {
-                    participant.classList.add('participantZoomed');
-                }
-                else {
-                    participant.classList.add('participantHidden');
-                }
-            }
-        });
-    }
-    else {
-        // zoom out
-        container.childNodes.forEach(participant => {
-            if (participant.classList && participant.classList.contains('participant')) {
-                participant.childNodes[0].childNodes.forEach(track => {
-                    if (track === trackElement) {
-                        track.classList.remove('trackZoomed');
-                    }
-                });
-                participant.classList.remove('participantZoomed')
-                participant.classList.remove('participantHidden')
-            }
-        });
-    }
-};
-
-function connectChat(token, conversationSid) {
-    return Twilio.Conversations.Client.create(token).then(_chat => {
-        chat = _chat;
-        return chat.getConversationBySid(conversationSid).then((_conv) => {
-            conv = _conv;
-            conv.on('messageAdded', (message) => {
-                addMessageToChat(message.author, message.body);
-            });
-            return conv.getMessages().then((messages) => {
-                chatContent.innerHTML = '';
-                for (let i = 0; i < messages.items.length; i++) {
-                    addMessageToChat(messages.items[i].author, messages.items[i].body);
-                }
-                toggleChat.disabled = false;
-            });
-        });
-    }).catch(e => {
-        console.log(e);
-    });
-};
-
-function addMessageToChat(user, message) {
-    chatContent.innerHTML += `<p><b>${user}</b>: ${message}`;
-    chatScroll.scrollTop = chatScroll.scrollHeight;
-}
-
-function toggleChatHandler() {
-    event.preventDefault();
-    if (root.classList.contains('withChat')) {
-        root.classList.remove('withChat');
-    }
-    else {
-        root.classList.add('withChat');
-        chatScroll.scrollTop = chatScroll.scrollHeight;
-    }
-};
-
-
-
-function onChatInputKey(ev) {
-    if (ev.keyCode == 13) {
-        conv.sendMessage(chatInput.value);
-        chatInput.value = '';
-    }
-};
-
-addLocalVideo();
-button.addEventListener('click', connectButtonHandler);
-shareScreen.addEventListener('click', shareScreenHandler);
-toggleChat.addEventListener('click', toggleChatHandler);
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
